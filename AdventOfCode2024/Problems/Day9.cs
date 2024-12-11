8bbd60bbc9a7ceee7b997afe7eccbcc6b2179481
tylerwigington <tyler.wigington.io@gmail.com>
2024-12-11 15:53:50 -0600

    day 9 wip

--- AdventOfCode2024/Problems/Day9.cs
+++ AdventOfCode2024/Problems/Day9.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2024.Problems;
+﻿using System.Text;
+
+namespace AdventOfCode2024.Problems;
 
 public static class Day9
 {
@@ -6,64 +8,103 @@ public static class Day9
     {
         using var file = File.OpenText("./Inputs/day9_test.txt");
 
+        //using var ms = new MemoryStream();
+        //await ms.WriteAsync(Encoding.UTF8.GetBytes("12345"));
+        //ms.Seek(0, SeekOrigin.Begin);
+        //using var file = new StreamReader(ms);
         var currId = 0;
         List<FileBlock> blocks = [];
+        List<int> fileBlocks = [];
         while (!file.EndOfStream)
         {
             var sizeBit = file.Read();
             if (sizeBit == -1) break;
 
-            var size = Convert.ToInt32((char)sizeBit);
-            var free = Convert.ToInt32((char)file.Read());
-            blocks.Add(new(currId, size, free));
+            var size = (int)char.GetNumericValue((char)sizeBit);
+
+            var freeBit = file.Read();
+            if (freeBit == -1)
+            {
+                fileBlocks.AddRange(Enumerable.Repeat(currId, size));
+                break;
+            }
+
+            var free = (int)char.GetNumericValue((char)freeBit);
+            fileBlocks.AddRange(Enumerable.Repeat(currId, size).Concat(Enumerable.Repeat(-1, free)));
+            //blocks.Add(new() { Id = currId, Size = size, FreeSpace = free });
             currId++;
         }
 
-        List<FileBlock> sorted = [];
-        Dictionary<int, Queue<FileBlock>> blockQueues = new();
-        while (blocks.Count > 0)
+        var forwardIndex = 0;
+        var reverseIndex = fileBlocks.Count - 1;
+        while (reverseIndex >= forwardIndex)
         {
-            var first = blocks.First();
-            if (blockQueues.TryGetValue(first.FreeSpace, out var queue) && queue.TryDequeue(out var block))
+            while (fileBlocks[forwardIndex] != -1)
             {
-                sorted.Add(first);
-                sorted.Add(block);
-                continue;
+                forwardIndex++;
             }
-            
-            var last = blocks.LastOrDefault();
-            
-            if (last.Length == first.FreeSpace)
+
+            while (fileBlocks[forwardIndex] == -1)
             {
-                sorted.Add(first);
-                sorted.Add(last);
-                blocks.RemoveAt(blocks.Count - 1);
-                continue;
+                fileBlocks[forwardIndex] = fileBlocks[reverseIndex];
+                forwardIndex++;
+                reverseIndex--;
             }
-            
-            while (last.Length != first.FreeSpace)
-            {
-                
-                if(!blockQueues.ContainsKey(last.Length))
-                    blockQueues.Add(last.Length, []);
-                blockQueues[last.Length].Enqueue(last);
-                blocks.RemoveAt(blocks.Count - 1);
-                last = blocks.Last();
-            }
-            sorted.AddRange([first, last]);
         }
 
-        PrintBlocks(sorted);
-        return blocks.Count;
-    }
-
-    private static void PrintBlocks(List<FileBlock> blocks)
-    {
-        foreach (var block in blocks)
+        var sum = 0;
+        for (var i = 0; i < fileBlocks.Count - 1; i++)
         {
-            
+            sum += fileBlocks[i] * i;
         }
+        return sum;
+
+        //List<FileBlock> sorted = [];
+        //FileBlock? curr;
+        //var lastQueue = new Queue<FileBlock>();
+        //lastQueue.Enqueue(blocks.Last());
+        //blocks.RemoveAt(blocks.Count - 1);
+        //while (blocks.Count > 0)
+        //{
+        //    curr = blocks.First();
+        //    var currLast = lastQueue.Peek();
+        //    while (!curr.Full)
+        //    {
+        //        if(currLast.Size - currLast.SizeUsed == 0)
+        //        {
+        //            lastQueue.Dequeue();
+        //            var newLast = blocks.Last();
+        //            lastQueue.Enqueue(newLast);
+        //            currLast = newLast;
+        //            blocks.RemoveAt(blocks.Count - 1);
+        //        }
+
+        //        var amountToFill = curr.FreeSpace - curr.FilledSpace.Count;
+        //        curr.FilledSpace.AddRange(Enumerable.Repeat(new FillBlock(currLast.Id), amountToFill));
+        //        currLast.SizeUsed += amountToFill;
+        //    }
+        //    sorted.Add(curr);
+        //    blocks.RemoveAt(0);
+        //}
+
+        //int sum = 0;
+        //var temp = sorted.SelectMany(s => Enumerable.Repeat(new FillBlock(s.Id), s.Size).Concat(s.FilledSpace)).ToList();
+        //for(int i = 0; i < temp.Count; i++)
+        //{
+        //    sum += i * temp[i].BlockId;
+        //}
+        //return sum;
     }
 }
 
-public sealed record FileBlock(int Id, int Length, int FreeSpace);
+public sealed class FileBlock
+{
+    public required int Id { get; init; }
+    public required int Size { get; init; }
+    public required int FreeSpace { get; init; }
+    public int SizeUsed { get; set; }
+    public List<FillBlock> FilledSpace { get; set; } = [];
+    public bool Full => FilledSpace.Count == FreeSpace;
+}
+
+public sealed record FillBlock(int BlockId);
